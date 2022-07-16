5ef7fe1f68ade2536b17468a0cb027566b65686c
Thales Zarzar <thales.zarzar@intelie.com.br>
2022-07-16 18:41:38 -0300

    started reading csv

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import styled from 'styled-components'
-import { fetchEntries } from './api/api';
+import Mapper from './api/mapper';
 
 function App() {
   const [entries, setEntries] = React.useState<Array<Entry>>([])
 
-  React.useEffect(function () {
-    fetchEntries()
-  }, [])
+  const reader = React.useMemo(() => new FileReader(), [])
+  React.useEffect(() => {
+    reader.onload = e => {
+      const csvText: string | undefined = e.target?.result?.toString()
+      if(csvText?.length) setEntries(Mapper.entry.fromCSV(csvText.slice(csvText.indexOf('\n') + 1)))
+    } 
+  }, [reader])
+
+  function onFileSelection(e: React.ChangeEvent<HTMLInputElement>): void {
+    const file = e.target.files ? e.target.files[0] : null
+    if(!file) return
+
+    reader.readAsText(file)
+  }
 
   return (
     <Container>
+      <input type="file" onChange={onFileSelection} accept=".csv" />
       <ul>
         {entries.map(entry => <li>
           {entry.date.toString()}, {entry.glucose}
--- src/api/mapper.ts
+++ src/api/mapper.ts
@@ -1,7 +1,27 @@
 const Mapper = {
     entry: {
-        fromCSV: function (): Array<Entry> {
-            return []
+        fromCSV: function (csvText: string): Array<Entry> {
+            const entries: Array<Entry> = []
+
+            try {
+                const rows = csvText.split('\n')
+                for(let i = 0; i < rows.length; i++) {
+                    const row = rows[i].split(',')
+                    const date = new Date(`${row[0]} ${row[1]}`)
+                    const glucose = parseFloat(row[2])
+                    const cho = parseFloat(row[3])
+                    const insulin = parseFloat(row[4])
+                    const comment = row[5]
+
+                    console.log(row)
+
+                    entries.push({ date, glucose, cho, insulin, comment })
+                    }
+            } catch(e) {
+                alert('can\'t read this file')
+            }
+            
+            return entries
         }
     }
 }
